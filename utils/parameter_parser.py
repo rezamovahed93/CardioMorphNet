0ecd1e44e74da97bbb870188081221876c45e834
Reza Akbari Movahed <32155999+rezamovahed93@users.noreply.github.com>
2026-06-07 12:00:52 +0100

    Update parameter_parser.py

--- utils/parameter_parser.py
+++ utils/parameter_parser.py
@@ -16,7 +16,7 @@ import argparse
 def parse_args_data_splitting():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--csv_paths", metavar='m2m_w', type=str, default="/data/MyPhD/My PHD Datasets/UKB Heart Images NIFTI/patient_data.csv" , help="The path of obatined CSV file for UKB dataset")
+    parser.add_argument("--csv_paths", metavar='m2m_w', type=str , help="The path of obatined CSV file for the dataset")
 
     args = parser.parse_args()
     return args
@@ -33,10 +33,10 @@ def parse_args_preprocess():
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
     optional_args = parser._action_groups.pop()
     
-    parser.add_argument("--data_path", metavar='dp', type=str, default=r"/data/MyPhD/My PHD Datasets/UKB Heart Images NIFTI/", help="The path of the UKB dataset in NIFTI format")    
-    parser.add_argument("--preprocessed_data_path", metavar='dp', type=str, default=r"/data/MyPhD/My PHD Datasets/UKB Heart Images NIFTI preprocessed/", help="The path for the preprocessed data" )
+    parser.add_argument("--data_path", metavar='dp', type=str, help="The path of the dataset in NIFTI format")    
+    parser.add_argument("--preprocessed_data_path", metavar='dp', type=str, help="The path for the preprocessed data" )
     parser.add_argument('--zero_pad_flag', action='store_true', help='A boolean flag for zero padding (True if specified, False otherwise)')
-    parser.add_argument("--csv_paths", metavar='m2m_w', type=str, default="/data/MyPhD/My PHD Datasets/UKB Heart Images NIFTI/patient_data.csv" , help="The path of obatined CSV file for UKB dataset")
+    parser.add_argument("--csv_paths", metavar='m2m_w', type=str , help="The path of obatined CSV file for dataset")
 
 
     parser._action_groups.append(optional_args)
@@ -61,7 +61,7 @@ def parse_args_train_reg_search():
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
     optional_args = parser._action_groups.pop()
     
-    parser.add_argument("--data_path", metavar='dp', type=str, default=r"/data/MyPhD/My PHD Datasets/UKB Heart Images Preprocessed Zero Padded", help="The path of the preprocessed UKB dataset")    
+    parser.add_argument("--data_path", metavar='dp', type=str, help="The path of the preprocessed the dataset")    
     parser.add_argument("--epoch_size", metavar='epcoh', type=int, default=1, help="Epoch size (default: 50)")
     parser.add_argument("--batch_size", metavar='bs', type=int, default=1, help="Batch size (default: 1)")
     parser.add_argument("--learning_rate", metavar='lr', type=float, default=0.001, help="Learning rate (default: 0.001)")
@@ -93,7 +93,7 @@ def parse_args_train():
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
     optional_args = parser._action_groups.pop()
     
-    parser.add_argument("--data_path", metavar='dp', type=str, default=r"/data/MyPhD/My PHD Datasets/UKB Heart Images Preprocessed Zero Padded", help="The path of the preprocessed UKB dataset")    
+    parser.add_argument("--data_path", metavar='dp', type=str, help="The path of the preprocessed dataset")    
     parser.add_argument("--epoch_size", metavar='epcoh', type=int, default=150, help="Epoch size (default: 50)")
     parser.add_argument("--batch_size", metavar='bs', type=int, default=1, help="Batch size (default: 1)")
     parser.add_argument("--learning_rate", metavar='lr', type=float, default=0.001, help="Learning rate (default: 0.001)")
@@ -125,7 +125,7 @@ def parse_args_test():
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
     optional_args = parser._action_groups.pop()
     
-    parser.add_argument("--data_path", metavar='dp', type=str, default=r"/data/MyPhD/My PHD Datasets/UKB Heart Images Preprocessed Zero Padded", help="The path of the preprocessed UKB dataset")    
+    parser.add_argument("--data_path", metavar='dp', type=str, help="The path of the preprocessed the dataset")    
     parser.add_argument("--batch_size", metavar='bs', type=int, default=1, help="Batch size (default: 1)")
     parser.add_argument("--model_weights_path", metavar='model_w', type=str,  default=r"C:\My PHD Codes\My Implementation\First Proposed Method\Final Implementation (1)\best_model_epoch(43) val(0.31).pth")
     parser.add_argument("--saving_results_path", metavar='results_path', type=str,  default=r"C:\My PHD Codes\My Implementation\First Proposed Method\Final Implementation (1)\results.pkl")
